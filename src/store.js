c81f5a8ceb4fa8b8dbe530652699017355665346
Raimond Fowler <raifowler@gmail.com>
2018-08-15 15:48:52 -0700

    Added getDerivedStateFromProps lifecycle method to clients to get clients from firestore. Added spinner component for loading graphic when page is loading.

---
+++ src/store.js
@@ -0,0 +1,55 @@
+import { createStore, combineReducers, compose } from "redux";
+import firebase from "firebase";
+import "firebase/firestore";
+import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
+import { reduxFirestore, firestoreReducer } from "redux-firestore";
+
+// Reducers @TODO
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCdGyrSvla6j2PzCOcGj9aKEZPmJjKm59U",
+  authDomain: "react-client-panel-8194b.firebaseapp.com",
+  databaseURL: "https://react-client-panel-8194b.firebaseio.com",
+  projectId: "react-client-panel-8194b",
+  storageBucket: "react-client-panel-8194b.appspot.com",
+  messagingSenderId: "811365033421"
+};
+
+// React-Redux-Firebase config
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+
+// Initialize firestore
+const firestore = firebase.firestore();
+
+// Add reactReduxFirebase enhancer when making store creator
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(firebase, rrfConfig), // firebase instance as first argument
+  reduxFirestore(firebase) // <- needed if using firestore
+)(createStore);
+
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer // <- needed if using firestore
+});
+
+// Create initial state
+const initialState = {};
+
+// Create store
+const store = createStoreWithFirebase(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
