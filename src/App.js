c452fe1b20b97f4052373472522c6e9547d6e65a
Raimond Fowler <raifowler@gmail.com>
2018-08-15 11:16:08 -0700

    second commit with UI started

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import AppNavBar from "./components/layout/AppNavBar";
+import Dashboard from "./components/layout/Dashboard";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppNavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
